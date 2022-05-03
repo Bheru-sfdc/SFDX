74b49e8ed7cf7e838660a242b08f3fb42c89f742
Bheru-sfdc <kbssalesforce77@gmail.com>
2022-05-03 11:24:39 +0530

    commit10

---
+++ buildScripts/PullGitRepo.sh
@@ -0,0 +1,4 @@
+#below git pull command actually is git pull origin BRANCH NAME from where build is to happen i.e. master/develop/feature etc.
+    git config user.email "kbssalesforce77@gmail.com"
+    git config user.name "Bheru-sfdc"
+    git pull origin << pipeline.git.branch >>
