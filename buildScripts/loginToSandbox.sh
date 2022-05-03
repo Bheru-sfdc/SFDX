66b3cd912ba5266bfeeaf589faa73926e5a5d036
Bheru-sfdc <kbssalesforce77@gmail.com>
2022-05-03 13:58:44 +0530

    commit49

--- buildScripts/loginToSandbox.sh
+++ buildScripts/loginToSandbox.sh
@@ -2,3 +2,4 @@ mkdir keys
     echo $Server_Key | base64 -di > keys/server.key
 echo "Authenticating org"
     sfdx force:auth:jwt:grant --clientid $Consumer_key --jwtkeyfile keys/server.key --username $SFDX_Username --instanceurl $Instance_URL --setdefaultdevhubusername -a DevHub
+    rm keys
