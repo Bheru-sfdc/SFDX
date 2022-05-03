da910c102e02104707224e1211317a75c20a87c1
Bheru-sfdc <kbssalesforce77@gmail.com>
2022-05-03 11:47:45 +0530

    commit14

--- buildScripts/PullGitRepo.sh
+++ buildScripts/PullGitRepo.sh
@@ -1,4 +1,4 @@
 echo "Set Head in Git Repo"
     git config user.email "kbssalesforce77@gmail.com"
     git config user.name "Bheru-sfdc"
-    git pull origin pipeline.git.branch
+    git pull origin "<<pipeline.git.branch>>"
